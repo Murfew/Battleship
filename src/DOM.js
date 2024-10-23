d75c532cd91e84f8f765c68c0888d5f0dd8b6bf8
Murfew <matthew.hurley953@gmail.com>
2024-10-23 16:27:21 -0400

    render the player's ship coordinates as gray

--- src/DOM.js
+++ src/DOM.js
@@ -18,12 +18,24 @@ export function renderPlayerBoard(player, isOpponent) {
   // reset board
   boardContainer.replaceChildren();
 
-  // render the board
+  // render the cells
   // first row and column for indices
+  renderCells(player, boardContainer);
+
+  // TODO render a player's ships
+  if (!isOpponent) {
+    renderShips(player);
+  }
+
+  // TODO render the misses
+  // TODO render the hits
+}
+
+function renderCells(player, DOMContainer) {
   for (let i = 0; i < player.board.size + 1; i++) {
     const boardRow = document.createElement("div");
     boardRow.setAttribute("class", "row");
-    boardContainer.append(boardRow);
+    DOMContainer.append(boardRow);
 
     for (let j = 0; j < player.board.size + 1; j++) {
       const boardCell = document.createElement("div");
@@ -40,12 +52,28 @@ export function renderPlayerBoard(player, isOpponent) {
         boardCell.setAttribute("class", "cell");
         boardCell.dataset.row = i;
         boardCell.dataset.col = j;
-        const hitOrMissDisplay = document.createElement("div");
-        hitOrMissDisplay.setAttribute("class", "hit-or-miss-display");
-        boardCell.append(hitOrMissDisplay);
+        const marker = document.createElement("div");
+        marker.setAttribute("class", "marker");
+        boardCell.append(marker);
       }
 
       boardRow.append(boardCell);
+    }
+  }
+}
+
+function renderShips(player) {
+  const board = player.board;
+
+  for (const obj of board.ships) {
+    const shipCoordinates = obj.coordinates;
+
+    for (const pair of shipCoordinates) {
+      const cell = document.querySelector(
+        `#player-board [data-col='${pair[0] + 1}'][data-row='${pair[1] + 1}']`
+      );
+
+      cell.classList.add("ship");
     }
   }
 }
